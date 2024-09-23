5cc92b8e5418aebc32c978d36d5fa34e69b9c375
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-09-23 10:40:08 +0530

    fix: fix loading state and add new password modal

---
+++ src/app/(auth)/auth/layout.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from "react";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+	return <div className="h-full w-full flex items-center justify-center">{children}</div>;
+}
